e89e38a2c68810ec423e6179fb2e14c8515cfcdc
bespalov0928 <bespalov0928@mail.ru>
2022-02-11 08:22:26 +0700

    3.4.9.1.2. Spring boot Webflux

---
+++ src/main/java/com/example/weather_reactive/Weather.java
@@ -0,0 +1,4 @@
+package com.example.weather_reactive;
+
+public class Weather {
+}
---
+++ src/main/java/com/example/weather_reactive/WeatherReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.weather_reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherReactiveApplication.class, args);
+    }
+
+}
